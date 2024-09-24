fe7d3c02db0a49ad400a92b4dbdea7ddc4e49152
Lukas Jenni <lukas.jenni@hotmail.de>
2024-09-24 22:15:45 +0200

    02 - ball solution

--- week2/ball/ball.js
+++ week2/ball/ball.js
@@ -16,13 +16,35 @@ function start() {
 
 function nextBoard() {
     // keep old ball values for the sake of efficient clearing of the old display
+     old.x = ball.x;
+     old.y = ball.y;
 
     // handle ball is hitting the bounds
     //   reverse direction
     //   lose some energy relative to the current inertia (only velocity varies)
+    if (ball.x - radius <= 0 ) { // hit left-border
+        ball.dx = -(ball.dx * 0.7);
+    }
+    else if (ball.x + radius >= 400 ) { //hit right-border
+        ball.dx = -(ball.dx * 0.7);
+    }
+    else if (ball.y + radius >= 400 ) {//hit bottom-border
+        ball.dy = -(ball.dy * 0.7);
+    }
 
     // calculate new position
+    ball.x = ball.x + ball.dx;
+    ball.y = ball.y + ball.dy;
     // calculate any changes in velocity due to gravitational pull or medium resistance
+    if( ball.dy < 0 && ball.dy > -0.2 ) { 
+        ball.dy = 0.2;
+    }
+    if( ball.dy > 0 && ball.dy < 0.2 ){
+        ball.dy = -0.2;
+    }
+    else {
+        ball.dy = ball.dy > 0 ? ball.dy * 1.3 : ball.dy * 0.7;
+    }
 
 
 }
