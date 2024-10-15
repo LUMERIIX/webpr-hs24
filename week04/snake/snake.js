cfc00a54eb15bab550d3378e069c388c5def9b02
Lukas Jenni <lukas.jenni@hotmail.de>
2024-10-15 15:06:29 +0200

    format

--- week04/snake/snake.js
+++ week04/snake/snake.js
@@ -2,112 +2,120 @@
 
 const MAX = 20;
 
-const north = Pair(0)(-1);
-const east = Pair(1)(0);
-const south = Pair(0)(1);
-const west = Pair(-1)(0);
+const north = Pair( 0)(-1);
+const east  = Pair( 1)( 0);
+const south = Pair( 0)( 1);
+const west  = Pair(-1)( 0);
 
 let direction = north;
 
 const clockwise = [north, east, south, west, north];
 const countercw = [north, west, south, east, north];
 
-const snake = [Pair(10)(5), Pair(10)(6), Pair(10)(7), Pair(10)(8)];
-let food = Pair(15)(15);
+const snake = [
+    Pair(10)(5),
+    Pair(10)(6),
+    Pair(10)(7),
+    Pair(10)(8),
+];
+let food    = Pair(15)(15);
 
 // function snakeEquals(a, b) { return a.x === b.x && a.y === b.y }
-const pairEq = (a) => (b) => a(fst) === b(fst) && a(snd) === b(snd); // todo: your code here
+// const pairEq = a => b =>  undefined; // todo: your code here
 
-// Pair + Pair = Pair        // MonoidPair
-const pairPlus = (a) => (b) => Pair(a(fst) + b(fst))(a(snd) + b(snd)); // todo: your code here
+const pairEq = a => b => a(fst) === b(fst) && a(snd) === b(snd);
+
+// Pair + Pair = Pair        // Monoid
+// const pairPlus = a => b =>  undefined; // todo: your code here
+const pairPlus = a => b =>  Pair (a(fst) + b(fst)) (a(snd) + b(snd));
 
 // Function and Pair = Pair  // Functor
-const pairMap = (f) => (p) => Pair(f(p(fst)))(f(p(snd))); // todo: your code here
+// const pairMap = f => p =>  undefined; // todo: your code here
+const pairMap = f => p =>  Pair (f(p(fst))) (f(p(snd)));
+
 
 function changeDirection(orientation) {
-  const idx = orientation.indexOf(direction);
-  direction = orientation[idx + 1];
+    const idx = orientation.indexOf(direction);
+    direction = orientation[idx + 1];
 }
 
 /**
  * when trying to get an element by id from the dom, the element might not be there
  * but in this case the application should not crash randomly
- * @return Either ErrorMessage or HTMLElement
- */
+* @return Either ErrorMessage or HTMLElement
+*/
 function safeGetElementById(id) {
-  const result = document.getElementById(id);
-  return result === null
-    ? Left("Element with id " + id + " not found")
-    : Right(result);
+    const result = document.getElementById(id);
+    return result === null; // todo: your code here
 }
 
-const log = (s) => console.log(s);
+const log = s => console.log(s);
 
 function start() {
-  safeGetElementById("canvas")((msg) => console.error(msg))((c) =>
-    startWithCanvas(c)
-  );
+
+    // todo: if safeGetElementById("canvas") yields an error message, log it. Otherwise startWithCanvas
+
 }
 
-const startWithCanvas = (canvas) => {
-  const context = canvas.getContext("2d");
+const startWithCanvas = canvas => {
 
-  const rightArrow = 39;
-  const leftArrow = 37;
-  window.onkeydown = (evt) => {
-    const orientation = evt.keyCode === rightArrow ? clockwise : countercw;
-    changeDirection(orientation);
-  };
+    const context = canvas.getContext("2d");
 
-  setInterval(() => {
-    nextBoard();
-    display(context);
-  }, 1000 / 5);
+    const rightArrow = 39;
+    const leftArrow  = 37;
+    window.onkeydown = evt => {
+        const orientation = (evt.keyCode === rightArrow) ? clockwise : countercw;
+        changeDirection(orientation);
+    };
+
+    setInterval(() => {
+        nextBoard();
+        display(context);
+    }, 1000 / 5);
 };
 
-const inBounds = (x) => {
-  if (x < 0) {
-    return MAX - 1;
-  }
-  if (x > MAX) {
-    return 0;
-  }
-  return x;
+const inBounds = x => {
+    if (x < 0)   { return MAX - 1 }
+    if (x > MAX) { return 0 }
+    return x
 };
 
 function nextBoard() {
-  const max = 20;
-  const oldHead = snake[0];
+    const max = 20;
+    const oldHead = snake[0];
 
-  const newHead = pairPlus(oldHead)(direction); // todo: your code here: old head plus direction
-  const head = Pair(inBounds(newHead(fst)))(inBounds(newHead(snd))); // todo: your code here: new head put in bounds
+    const newHead = undefined; // todo: your code here: old head plus direction
+    const head    = undefined; // todo: your code here: new head put in bounds
 
-  const pickRandom = () => Math.floor(Math.random() * max);
-  if (pairEq(food)(head)) {
-    // todo: have we found any food?
-    food = Pair(pickRandom())(pickRandom());
-  } else {
-    snake.pop(); // no food found => no growth despite new head => remove last element
-  }
+    const pickRandom = () => Math.floor(Math.random() * max);
+    if (true) {  // todo: have we found any food?
+        food = Pair(pickRandom())(pickRandom());
+    } else {
+        snake.pop(); // no food found => no growth despite new head => remove last element
+    }
 
-  snake.unshift(head); // put head at front of the list
+    snake.unshift(head); // put head at front of the list
 }
 
 function display(context) {
-  // clear
-  context.fillStyle = "black";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  // draw all elements
-  context.fillStyle = "cyan";
-  snake.forEach((element) => fillBox(context, element));
-  // draw head
-  context.fillStyle = "green";
-  fillBox(context, snake[0]);
-  // draw food
-  context.fillStyle = "red";
-  fillBox(context, food);
+    // clear
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // draw all elements
+    context.fillStyle = "cyan";
+    snake.forEach(element =>
+        fillBox(context, element)
+    );
+    // draw head
+    context.fillStyle = "green";
+    fillBox(context, snake[0]);
+    // draw food
+    context.fillStyle = "red";
+    fillBox(context, food);
 }
 
 function fillBox(context, element) {
-  context.fillRect(element(fst) * 20 + 1, element(snd) * 20 + 1, 18, 18);
+    context.fillRect(element(fst) * 20 + 1, element(snd) * 20 + 1, 18, 18);
 }
+
+
--- week04/snake/snakeTest.js
+++ week04/snake/snakeTest.js
@@ -1,48 +1,50 @@
+
 const snakeOk = [];
 
 //  before start, snake is in start position
-snakeOk.push(pairEq(snake[0])(Pair(10)(5)));
-// test pairAddition
-const resPair = Pair(20)(10);
-snakeOk.push(pairEq(pairPlus(Pair(10)(5))(Pair(10)(5)))(resPair));
+    snakeOk.push(pairEq (Pair(10)(5)) (Pair(10)(5)));
+    snakeOk.push(pairEq(snake[0]) (Pair(10)(5)));
 
-// test pairMap
-const resPair2 = Pair(2)(4);
-snakeOk.push(pairEq(resPair2)(pairMap((x) => x * 2)(Pair(1)(2))));
+    const resultPair1 = pairPlus(Pair(10)(5)) (Pair(10)(5));
+    snakeOk.push(pairEq(resultPair1) (Pair(20)(10)));
 
+    const resultPair2 = Pair(2)(4);
+    snakeOk.push( pairEq (resultPair2) ( pairMap (x=>x*2) (Pair(1)(2)) ));
+
+/*
 //  after one step, snake has moved up
-nextBoard();
-snakeOk.push(pairEq(snake[0])(Pair(10)(4)));
+    nextBoard();
+    snakeOk.push(pairEq(snake[0]) (Pair(10)(4)));
 
 //  before eating food, snake is of size 4
-snakeOk.push(snake.length === 4);
+    snakeOk.push(snake.length === 4);
 
 //  after eating food, snake has grown in size
-food = Pair(10)(3);
-nextBoard();
-snakeOk.push(snake.length === 5);
+    food = Pair(10)(3);
+    nextBoard();
+    snakeOk.push(snake.length === 5);
 
 //  current direction is north
-snakeOk.push(direction === north);
+    snakeOk.push(direction === north);
 
 //  when I go clockwise, I end up east
-changeDirection(clockwise);
-snakeOk.push(direction === east);
+    changeDirection(clockwise);
+    snakeOk.push(direction === east);
 
 //  going counterclockwise, we end up north again
-changeDirection(countercw);
-snakeOk.push(direction === north);
-
+    changeDirection(countercw);
+    snakeOk.push(direction === north);
+*/
 // test result report
-if (snakeOk.every((elem) => elem)) {
-  document.writeln("All " + snakeOk.length + " tests ok.");
+if ( snakeOk.every(id) ) {
+    document.writeln("All " + snakeOk.length + " tests ok.");
 } else {
-  document.writeln("Not all tests ok! Details:");
-  for (let i = 0; i < snakeOk.length; i++) {
-    if (snakeOk[i]) {
-      document.writeln("Test " + i + " ok");
-    } else {
-      document.writeln("Test " + i + " failed");
+    document.writeln("Not all tests ok! Details:");
+    for (let i = 0; i < snakeOk.length; i++) {
+        if(snakeOk[i]) {
+            document.writeln("Test "+ i +" ok");
+        } else {
+            document.writeln("Test "+ i +" failed");
+        }
     }
-  }
 }
