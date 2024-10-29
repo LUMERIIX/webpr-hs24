a4ac66b97b6c393c221be0a0cf6038a2cd9e90d9
LUMERIIX <lukas.jenni@hotmail.de>
2024-10-29 18:04:00 +0100

    notes week7

--- week07/util/util.js
+++ week07/util/util.js
@@ -1,8 +1,10 @@
-
 // todo: implement the times function
 
-Number.prototype.times = function(callback) {
-    const result = [];
-    for(let i=0; i<this; i++) { result.push(callback(i)) }
-    return result;
+Number.prototype.times = function (callback) {
+  //const result = [];
+  //for (let i = 0; i < this; i++) {
+  //  result.push(callback(i));
+  //}
+  //return result;
+  return Array.from({ length: this }, (_, i) => callback(i));
 };
